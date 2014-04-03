6213b566f52931476215d8aa35aab41f1af25063
Elan Perach <elanperach@gmail.com>
2014-04-04 01:40:18 +0300

    rtl working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     @currency_name ||= Setting[:currency_name]
   end
 
+  def rtl?
+    I18n.locale == :he
+  end
+
+  def current_locale
+    I18n.locale
+  end
+
+  def top_bar_direction
+    I18n.locale == :he ? 'left' : 'right'
+  end
+
 end

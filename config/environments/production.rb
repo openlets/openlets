200f89c2b1284de47197127caf82e716e33eafd9
Elan Perach <elanperach@gmail.com>
2014-05-29 19:28:36 +0300

    more trying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ OpenLets::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { :host => 'openlets.org' }
 

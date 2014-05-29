ef68da1fd8e32f7d11a00ddddc3f468d8cbda49e
Elan Perach <elanperach@gmail.com>
2014-05-29 19:06:33 +0300

    added mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ OpenLets::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'openlets-saas.herokuapp.com' }
+
   # Enable threaded mode
   # config.threadsafe!
 

56b7e3d2a0ea0849afa4de31de2df271e495af74
Elan Perach <elanperach@gmail.com>
2014-06-01 16:58:16 +0300

    no chosen in mobile

--- app/assets/javascripts/multi_select.js
+++ app/assets/javascripts/multi_select.js
@@ -1,9 +1,4 @@
 jQuery(document).ready(function($) {
-  // enable chosen.js
-  $('.chzn-select').chosen({
-    allow_single_deselect: true,
-    no_results_text: 'No results matched'
-  });
 
   $('a.auto-submit').click(function(){
     $("form#search-form").submit();
